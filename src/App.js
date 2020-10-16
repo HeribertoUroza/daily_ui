904e2d65e747f99ad86549385ed7cbc060a1550d
Heriberto Uroza <heribertouroza@pursuit.org>
2020-10-16 14:20:41 -0400

    styling for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+// COMPONENT
+import NavBar from './component/NavBar.js';
+
+// CONTAINERS
+import _001 from './container/001/001.js';
 
 function App() {
   return (
     <>
-      Hello World
+      <HashRouter>
+      <div className='app_container'>
+        <NavBar />
+        
+          <Switch>
+            <Route path='/001' component={_001} />
+          </Switch>
+        
+      </div>
+      </HashRouter>
     </>
   );
 }
