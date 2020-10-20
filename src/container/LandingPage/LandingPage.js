699dd74e141d32ae057b8da61a817737a94579f2
Heriberto Uroza <heribertouroza@pursuit.org>
2020-10-20 16:43:33 -0400

    added stylings to the landing page to introduce myself and challenge

--- src/container/LandingPage/LandingPage.js
+++ src/container/LandingPage/LandingPage.js
@@ -10,6 +10,8 @@ function LandingPage() {
                     Hello there! My name is Heriberto and this is my UI Daily Challenge.
                     <br/>
                     Click the tabs above to see the different UI's I created along with their description.
+                    <br/>
+                    My <strong className='landing_bold'>Goal</strong> is to practice CSS and improve my UI creating skills with each design.
                 </div>
             </div>
         </>
