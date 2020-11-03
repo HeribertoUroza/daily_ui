243205e7af53ae7f2af4c19abee6f5c5279c23f0
Heriberto Uroza <heribertouroza@pursuit.org>
2020-11-03 09:18:02 -0500

    added 001 design

--- src/container/001/001.js
+++ src/container/001/001.js
@@ -36,7 +36,7 @@ function _001() {
                         </div>
                         <div className='cont_container'>
                             <div className='_001_content'>
-                                <div className='_001_product'>
+                                <div className='_001_product1'>
                                     <img 
                                         src='https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
                                         className='_001_image'
@@ -44,12 +44,33 @@ function _001() {
                                     />
                                     <p className='_001_bio_1'>Register for a chance to WIN these next gen headphones!</p>
                                 </div>
+                                <div className='_001_product2'>
+                                    <img
+                                        src='https://images.unsplash.com/photo-1505740106531-4243f3831c78?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'
+                                        className='_001_image'
+                                        alt='product'
+                                    />
+                                    <p className='_001_bio_2'>Active Noise Cancellation, Comfortable, 360 Audio</p>
+                                </div>
+                                
+                                <div className='_001_product2'>
+                                    <img
+                                        src='https://images.unsplash.com/photo-1505751171710-1f6d0ace5a85?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+                                        className='_001_image _001_image1'
+                                        alt='product'
+                                    />
+                                
+                                </div>
                                 <form className='_001_form'>
                                     
-                                    <input />
-                                    <span>Email</span> 
+                                    <input 
+                                        className='_001_input' 
+                                        type='email'
+                                        
+                                        />
+                                    <span className='_001_label'>Email</span> 
                                     
-                                    <button>Register</button>
+                                    <button className='_001_button' onClick={(e)=> e.preventDefault()}>Register</button>
                                 </form>
                             </div>
                         </div>
